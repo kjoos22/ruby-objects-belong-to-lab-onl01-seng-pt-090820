c3d22f3d2db8f693837a587be8c8ca64716ec596
Kristian Joos <kjoos22@gmail.com>
2020-10-01 22:28:10 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,8 +1,8 @@
 class Song
-  attr_accessor :name, :artist
+  attr_accessor :title, :artist
   
-  def initialize(name, artist)
-    @name = name
+  def initialize(title, artist)
+    @title = title
     @artist = artist
   end
 end
