7470fd0df7ee522e73d363d9528533afef9efb05
Kristian Joos <kjoos22@gmail.com>
2020-10-01 22:36:17 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,10 +1,15 @@
 require 'pry'
 class Song
   attr_accessor :title, :artist
+  @@all = []
   
   def initialize
     binding.pry
     @title = title
     @artist = artist
+  end
+  
+  def self.all
+    @@all
   end
 end
